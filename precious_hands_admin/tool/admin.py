e67dec0daf5f0e5983c630137f1570b81769ddf8
ChristianGroeber <40316753+ChristianGroeber@users.noreply.github.com>
2019-03-21 11:55:27 +0100

    created models

--- precious_hands_admin/tool/admin.py
+++ precious_hands_admin/tool/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from tool.models import User, Child, Donor, PaymentInterval
+
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Child)
+admin.site.register(Donor)
+admin.site.register(PaymentInterval)
--- precious_hands_admin/tool/models.py
+++ precious_hands_admin/tool/models.py
@@ -1,3 +1,43 @@
+from django import forms
 from django.db import models
 
 # Create your models here.
+from django.db.models import ForeignKey
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    email_address = models.CharField(max_length=300, default="")
+    password = models.CharField(max_length=200, default="1234")
+    is_admin = models.BooleanField(default=False)
+
+
+class Child(models.Model):
+    name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    birthday = models.DateField()
+    image = models.ImageField(blank=True)
+
+
+class Donor(models.Model):
+    name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    email_address = models.CharField(max_length=300, default="")
+    road = models.CharField(max_length=100)
+    house_number = models.CharField(max_length=10)
+    plz = models.CharField(max_length=4)
+    city = models.CharField(max_length=50)
+
+
+class PaymentInterval(models.Model):
+    description = models.CharField(max_length=20)
+    amount_months = models.IntegerField()
+
+
+class DonationPlan(models.Model):
+    donor = ForeignKey(Donor, on_delete=models.CASCADE)
+    child = ForeignKey(Child, on_delete=models.CASCADE)
+    interval = ForeignKey(PaymentInterval, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    until = models.DateField()
