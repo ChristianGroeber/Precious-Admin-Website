9c2a538ccdf49273640a7932636b01bd80fddca2
ChristianGroeber <40316753+ChristianGroeber@users.noreply.github.com>
2019-03-21 12:24:16 +0100

    created creation sites

--- precious_hands_admin/tool/views.py
+++ precious_hands_admin/tool/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
-
+from .forms import CreateChild, CreateDonationPlan, CreateDonor
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'tool/index.html')
+
+
+def create(request, option):
+    if request.method == 'POST':
+        form = CreateChild(request.POST)
+        if option is 'donor':
+            form = CreateDonor(request.POST)
+        elif option is 'donation_plan':
+            form = CreateDonationPlan(request.POST)
+        if form.is_valid():
+            form_data = form.save(commit=False)
+            form_data.save()
+            return redirect('index')
+    else:
+        form = CreateChild()
+        if option is 'donor':
+            form = CreateDonor()
+        elif option is 'donation_plan':
+            form = CreateDonationPlan()
+        return render(request, 'tool/create.html', {'form': form})
