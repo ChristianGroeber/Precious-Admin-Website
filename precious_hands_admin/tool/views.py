96b37358e780dc99da67670b86a57a95f67d6ff6
ChristianGroeber <christian.groeber@bluewin.ch>
2019-03-25 11:45:06 +0100

    small changes

--- precious_hands_admin/tool/views.py
+++ precious_hands_admin/tool/views.py
@@ -18,6 +18,7 @@ def user_login(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
+        print(user)
         if user is not None:
             login(request, user)
             return redirect('index')
