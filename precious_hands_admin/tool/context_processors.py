fb08d8032b08cad8f0f2fea703856caa88ab41b7
christiangroeber <christian.groeber@bluewin.ch>
2019-06-13 14:34:55 +0200

    added context processors and profile image to base

---
+++ precious_hands_admin/tool/context_processors.py
@@ -0,0 +1,12 @@
+from .models import MyUser
+
+
+def profile_image(request):
+    user = request.user
+    print(user)
+    for my_user in MyUser.objects.all():
+        if str(my_user.user) == str(user):
+            return {'profile_image': my_user.profile_picture}
+    else:
+        print('nothing found')
+        return {}
