284818669e2671bb8d4564d801b9e99362081785
christiangroeber <christian.groeber@bluewin.ch>
2019-06-13 17:43:04 +0200

    styled the account image and popup

--- precious_hands_admin/tool/context_processors.py
+++ precious_hands_admin/tool/context_processors.py
@@ -6,7 +6,7 @@ def profile_image(request):
     print(user)
     for my_user in MyUser.objects.all():
         if str(my_user.user) == str(user):
-            return {'profile_image': my_user.profile_picture}
+            return {'profile_image': my_user.profile_picture, 'user': user}
     else:
         print('nothing found')
         return {}
